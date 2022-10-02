9f3fb67c77c9f19e5f6614fc9a179aeb4335e683
lipeeeee <filipe.pereirafga@gmail.com>
2022-10-02 22:45:19 +0100

    .gitignore test

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from question import Question
 
 def main():
     new_question = Question()
-    print(new_question.door_map)
 
 if __name__ == '__main__':
     # only run if compiler started in main.py
